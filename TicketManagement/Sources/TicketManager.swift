1c874790aab88b2d4cb133aa011c13ffe05a357f
Dũng Đặng <junestchick@users.noreply.github.com>
2019-01-10 10:47:41 +0700

    Update TicketManager.swift
    Add document.

--- TicketManagement/Sources/TicketManager.swift
+++ TicketManagement/Sources/TicketManager.swift
@@ -9,27 +9,50 @@
 import Foundation
 import UIKit
 
+
+/// Enum that defines status of ticket.
+///
+/// - success: success status.
+/// - failed: failed status.
 public enum TicketStatus {
+    /// - success: successfully status, with success message.
     case success(String)
+    
+    /// - failed: failed status, with error message.
     case failed(String)
 }
 
+
+/// The delegate returns result of SDK actions.
 public protocol TicketManageDelegate {
+    /// Return when Ticket booking has been cancelled.
     func canceled()
+    
+    
+    /// Return when Ticket booking has been booked successfully, includes status.
+    ///
+    /// - Parameter status: status of ticket.
     func finish(with status: TicketStatus)
 }
 
+
+/// class that manages Ticket Booking flows.
 public class TicketManager {
     
     var delegate: TicketManageDelegate?
     var presentViewController: UIViewController?
     var isAuthenticated = false
     
-    public init(viewcontroller: UIViewController) {
-        self.presentViewController = viewcontroller
-        self.delegate = viewcontroller as? TicketManageDelegate
+    
+    /// Init TicketManager with paired viewController, this controller will present TicketManager screens.
+    ///
+    /// - Parameter viewcontroller: paired viewController.
+    public init(viewController: UIViewController) {
+        self.presentViewController = viewController
+        self.delegate = viewController as? TicketManageDelegate
     }
     
+    /// Open Ticket Management Flow, if user is not logged in before, the SDK will show login screen, otherwise open booking ticket screen.
     open func openBookingTicketViewController() {
         print("openBookingTicketViewController")
         if isAuthenticated {
@@ -57,11 +80,11 @@ public class TicketManager {
 }
 
 extension TicketManager: LoginViewControllerDelegate {
-    public func canceled() {
+    func canceled() {
         delegate?.canceled()
     }
-    
-    public func loginSuccess(with userId: String) {
+
+    func loginSuccess(with userId: String) {
         isAuthenticated = true
         showBookTicketView()
     }
